90c223245b5339eed12557f362baa7a99e9d17f9
Tom Vajtay <vajtayt@gmail.com>
2019-07-17 14:11:23 -0400

    Adding Slurm and analysis scripts

---
+++ SLURM/caiman_slurm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Shell script used to submit a SLURM job with SBATCH
+#You would call this shell script with SBATCH
+#"sbatch /path/to/script/caiman_slurm.sh"
+
+
+#SBATCH --partition=main             # Partition (job queue)
+#SBATCH --no-requeue                 # Do not re-run job  if preempted
+#SBATCH --job-name=caiman_naomi      # Assign an short name to your job
+#SBATCH --nodes=1                    # Number of nodes you require
+#SBATCH --ntasks=1                   # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=15           # Number of requested cores (Max @ Rutgers 30/node)
+#SBATCH --mem=60000                  # Real memory (RAM) required (MB) (Max @ Rutgers is 120000/node)
+#SBATCH --time=30:00:00              # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out     # STDOUT output file
+#SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
+#SBATCH --export=ALL                 # Export your current env to the job env, not neccessary but good to know
+
+WORKING_DIRECTORY='/scratch/tjv55/data' # !!! You need to change this to the parent directory of files to be analyzed
+cd $WORKING_DIRECTORY  # go to the parent directory
+source activate caiman #Activate your anaconda environment you have installed previously on the HPC
+
+#Declaring environment variables to make sure CaImAn runs efficiently
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+#THE MAGIC HAPPENS HERE
+#First we use the internal BASH command "find" to search current and subdirectories for tif files
+#then we "execdir" within "find" so that on each file it finds it runs the caiman pipeline and saves
+#the data back in its respective directory, "{}" represents the filename to be loaded.
+#'slurm_pipeline.py' takes a single CLI argument which is the file to be analyzed.
+find -type f -name *.tif -execdir python /home/tjv55/slurm_pipeline.py {} \;
+
+#The following would be the variation of the previous command if you didn't want to include
+#the parent directory itself in the search just the sub-directories.
+
+#find . -type f -name *.tif -execdir python /home/tomi/CaImAn/SLURM/slurm_pipeline.py {} \;
---
+++ SLURM/slurm_pipeline.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+
+"""
+Complete demo pipeline for processing two photon calcium imaging data using the
+CaImAn batch algorithm. The processing pipeline included motion correction,
+source extraction and deconvolution. The demo shows how to construct the
+params, MotionCorrect and cnmf objects and call the relevant functions. You
+can also run a large part of the pipeline with a single method (cnmf.fit_file)
+
+This demo pertains to two photon data. For a complete analysis pipeline for
+one photon microendoscopic data see demo_pipeline_cnmfE.py
+
+copyright GNU General Public License v2.0
+authors: @agiovann and @epnev
+
+This pipeline demo has since been modified to take CLI arguments as part of a
+SLURM script and save labeled images of the final detected components in the
+origin directory for each respective movie in addition to the HDF5 file and
+a AVI of the denoised components.
+@tvajtay
+"""
+
+import cv2
+import glob
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import argparse
+from pathlib import Path
+import imageio
+
+#Set up argparse to take filename as CLI argument and then store as the 'in_file' component of args
+parser = argparse.ArgumentParser(description='Video filename and location')
+parser.add_argument('in_file', type=str, help='Input file name')
+args = parser.parse_args()
+
+try:
+    cv2.setNumThreads(0)
+except:
+    pass
+
+try:
+    if __IPYTHON__:
+        # this is used for debugging purposes only. allows to reload classes
+        # when changed
+        get_ipython().magic('load_ext autoreload')
+        get_ipython().magic('autoreload 2')
+except NameError:
+    pass
+
+
+import caiman as cm
+from caiman.motion_correction import MotionCorrect
+from caiman.source_extraction.cnmf import cnmf as cnmf
+from caiman.source_extraction.cnmf import params as params
+
+# %%
+# Set up the logger (optional); change this if you like.
+# You can log to a file using the filename parameter, or make the output more
+# or less verbose by setting level to logging.DEBUG, logging.INFO,
+# logging.WARNING, or logging.ERROR
+
+logging.basicConfig(format=
+                    "%(relativeCreated)12d [%(filename)s:%(funcName)20s():%(lineno)s]"\
+                    "[%(process)d] %(message)s",
+                    level=logging.INFO)
+
+#%%
+def main():
+    pass  # For compatibility between running under Spyder and the CLI
+
+#%% Select file from input arguments
+    fnames = args.in_file  # filename to be processed from argparse
+
+#%% First setup some parameters for data and motion correction
+
+    # dataset dependent parameters
+    fr = 15.49             # imaging rate in frames per second
+    decay_time = 0.4    # length of a typical transient in seconds
+    dxy = (1.5, 1.5)      # spatial resolution in x and y in (um per pixel)
+    # note the lower than usual spatial resolution here
+    max_shift_um = (12., 12.)       # maximum shift in um
+    patch_motion_um = (100., 100.)  # patch size for non-rigid correction in um
+
+    # motion correction parameters
+    pw_rigid = True       # flag to select rigid vs pw_rigid motion correction
+    # maximum allowed rigid shift in pixels
+    max_shifts = [int(a/b) for a, b in zip(max_shift_um, dxy)]
+    # start a new patch for pw-rigid motion correction every x pixels
+    strides = tuple([int(a/b) for a, b in zip(patch_motion_um, dxy)])
+    # overlap between pathes (size of patch in pixels: strides+overlaps)
+    overlaps = (24, 24)
+    # maximum deviation allowed for patch with respect to rigid shifts
+    max_deviation_rigid = 3
+
+    mc_dict = {
+        'fnames': fnames,
+        'fr': fr,
+        'decay_time': decay_time,
+        'dxy': dxy,
+        'pw_rigid': pw_rigid,
+        'max_shifts': max_shifts,
+        'strides': strides,
+        'overlaps': overlaps,
+        'max_deviation_rigid': max_deviation_rigid,
+        'border_nan': 'copy'
+    }
+
+    opts = params.CNMFParams(params_dict=mc_dict)
+
+# %% start a cluster for parallel processing
+    c, dview, n_processes = cm.cluster.setup_cluster(
+        backend='local', n_processes=None, single_thread=False)
+
+# %%% MOTION CORRECTION
+    # first we create a motion correction object with the specified parameters
+    mc = MotionCorrect(fnames, dview=dview, **opts.get_group('motion'))
+    # note that the file is not loaded in memory
+
+# %% Run (piecewise-rigid motion) correction using NoRMCorre
+    mc.motion_correct(save_movie=True)
+
+# %% MEMORY MAPPING
+    border_to_0 = 0 if mc.border_nan is 'copy' else mc.border_to_0
+    # you can include the boundaries of the FOV if you used the 'copy' option
+    # during motion correction, although be careful about the components near
+    # the boundaries
+
+    # memory map the file in order 'C'
+    fname_new = cm.save_memmap(mc.mmap_file, base_name='memmap_', order='C',
+                               border_to_0=border_to_0)  # exclude borders
+
+    # now load the file
+    Yr, dims, T = cm.load_memmap(fname_new)
+    images = np.reshape(Yr.T, [T] + list(dims), order='F')
+    # load frames in python format (T x X x Y)
+
+# %% restart cluster to clean up memory
+    cm.stop_server(dview=dview)
+    c, dview, n_processes = cm.cluster.setup_cluster(
+        backend='local', n_processes=None, single_thread=False)
+
+# %%  parameters for source extraction and deconvolution
+    p = 1                    # order of the autoregressive system
+    gnb = 2                  # number of global background components
+    merge_thr = 0.85         # merging threshold, max correlation allowed
+    rf = 15                  # half-size of the patches in pixels. e.g., if rf=25, patches are 50x50
+    stride_cnmf = 6          # amount of overlap between the patches in pixels
+    K = 4                    # number of components per patch
+    gSig = [5, 5]            # expected half size of neurons in pixels
+    method_init = 'greedy_roi'   # initialization method (if analyzing dendritic data using 'sparse_nmf')
+    ssub = 2                     # spatial subsampling during initialization
+    tsub = 2                     # temporal subsampling during intialization
+    s_min = -100                   # minimum signal amplitude needed in order for a transient to be considered as activity
+    min_size_neuro = 0.1*gSig[0]*np.pi**2
+    max_size_neuro = 2.5*gSig[0]*np.pi**2
+
+    # parameters for component evaluation
+    opts_dict = {'fnames': fnames,
+                 'fr': fr,
+                 'nb': gnb,
+                 'rf': rf,
+                 'K': K,
+                 'gSig': gSig,
+                 'stride': stride_cnmf,
+                 'method_init': method_init,
+                 'rolling_sum': True,
+                 'merge_thr': merge_thr,
+                 'n_processes': n_processes,
+                 'only_init': True,
+                 'ssub': ssub,
+                 'tsub': tsub,
+                 'min_size_neuro': min_size_neuro,
+                 'max_size_neuro': max_size_neuro,
+                 's_min': s_min}
+
+    opts.change_params(params_dict=opts_dict)
+
+# %% RUN CNMF ON PATCHES
+    # First extract spatial and temporal components on patches and combine them
+    # for this step deconvolution is turned off (p=0)
+    opts.change_params({'p': 0})
+    cnm = cnmf.CNMF(n_processes, params=opts, dview=dview)
+    cnm = cnm.fit(images)
+
+# %% ALTERNATE WAY TO RUN THE PIPELINE AT ONCE
+    #   you can also perform the motion correction plus cnmf fitting steps
+    #   simultaneously after defining your parameters object using
+    #  cnm1 = cnmf.CNMF(n_processes, params=opts, dview=dview)
+    #  cnm1.fit_file(motion_correct=True)
+
+
+# %% RE-RUN seeded CNMF on accepted patches to refine and perform deconvolution
+    cnm.params.change_params({'p': p})
+    cnm2 = cnm.refit(images, dview=dview)
+    # %% COMPONENT EVALUATION
+    # the components are evaluated in three ways:
+    #   a) the shape of each component must be correlated with the data
+    #   b) a minimum peak SNR is required over the length of a transient
+    #   c) each shape passes a CNN based classifier
+    min_SNR = 2  # signal to noise ratio for accepting a component
+    rval_thr = 0.85  # space correlation threshold for accepting a component
+    cnn_thr = 0.99  # threshold for CNN based classifier
+    cnn_lowest = 0.2 # neurons with cnn probability lower than this value are rejected
+
+    cnm2.params.set('quality', {'decay_time': decay_time,
+                               'min_SNR': min_SNR,
+                               'rval_thr': rval_thr,
+                               'use_cnn': True,
+                               'min_cnn_thr': cnn_thr,
+                               'cnn_lowest': cnn_lowest})
+
+    cnm2.estimates.evaluate_components(images, cnm2.params, dview=dview)
+
+    #%% Extract DF/F values
+    cnm2.estimates.detrend_df_f(quantileMin=8, frames_window=250)
+
+    #%% update object with selected components
+    #cnm2.estimates.select_components(use_object=True)
+
+    cnm2.estimates.threshold_spatial_components(maxthr=0.3)
+    cnm2.estimates.remove_small_large_neurons(min_size_neuro=min_size_neuro, max_size_neuro=max_size_neuro)
+
+    # %% plot contours of found components
+    Cn = cm.local_correlations(images, swap_dim=False)
+    Cn[np.isnan(Cn)] = 0
+    cnm2.estimates.plot_contours(img=Cn, idx=cnm2.estimates.idx_components, display_numbers=True)
+    plt.title(Path(args.in_file).stem)
+    plt.savefig(Path(args.in_file).stem + '.png', dpi=400)
+
+    #Using Path we get the base name of the video and use the same name for H5
+    sname = Path(args.in_file).stem + '.hdf5'
+    cnm2.save(sname)
+
+
+    #%% STOP CLUSTER and clean up log files
+    cm.stop_server(dview=dview)
+    log_files = glob.glob('*_LOG_*')
+    for log_file in log_files:
+        os.remove(log_file)
+
+    #Create denoised video as matrix
+    denoised = cm.movie(cnm2.estimates.A.dot(cnm2.estimates.C) + \
+                    cnm2.estimates.b.dot(cnm2.estimates.f)).reshape(dims + (-1,), order='F').transpose([2, 0, 1])
+
+    #Normalizing denoised matrix to the [0,255] range prior to type conversion
+    max_pixel = np.nanmax(denoised)
+    norm_denoised = (denoised/max_pixel)*255
+
+    #Type conversion so imageio doesn't get mad
+    norm_denoised = np.uint8(norm_denoised)
+
+    #Saving a denoised avi of the analyzed video
+    sname = Path(args.in_file).stem + '.avi'
+    imageio.mimwrite(sname, norm_denoised, fps=15.49)
+
+# %%
+# This is to mask the differences between running this demo in Spyder
+# versus from the CLI
+if __name__ == "__main__":
+    main()
